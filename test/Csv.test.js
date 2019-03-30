d7af2a6b115af8b2679fd81b6422d596a035dbcf
Carlos Azaustre <carlos.azaustre@ibm.com>
2019-03-30 23:55:55 +0100

    add eslint

--- test/Csv.test.js
+++ test/Csv.test.js
@@ -1,12 +1,12 @@
 const csv = require('../lib/Csv');
 
-test('Should return a csv format from an array of object', () =>{
+test('Should return a csv format from an array of object', () => {
   const data = [
-    { foo: "bar", choo: "moo" },
-    { foo: "ber", choo: "muu" }
+    { foo: 'bar', choo: 'moo' },
+    { foo: 'ber', choo: 'muu' },
   ];
 
   const result = 'foo,choo\nbar,moo\nber,muu\n';
 
   expect(csv(data)).toBe(result);
-})
+});
