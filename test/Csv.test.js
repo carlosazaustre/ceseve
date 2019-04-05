94480ed62f10f0d027a52a40bddbbe5025becdc8
Carlos Azaustre <cazaustre@gmail.com>
2019-04-06 00:22:47 +0200

    add separator option

--- test/Csv.test.js
+++ test/Csv.test.js
@@ -1,12 +1,16 @@
 const csv = require('../lib/Csv');
 
+const mockData = [
+  { foo: 'bar', choo: 'moo' },
+  { foo: 'ber', choo: 'muu' },
+];
+
 test('Should return a csv format from an array of object', () => {
-  const data = [
-    { foo: 'bar', choo: 'moo' },
-    { foo: 'ber', choo: 'muu' },
-  ];
-
   const result = 'foo,choo\nbar,moo\nber,muu\n';
+  expect(csv(mockData)).toBe(result);
+});
 
-  expect(csv(data)).toBe(result);
+test('Should return a csv splitted by semicolons', () => {
+  const result = 'foo;choo\nbar;moo\nber;muu\n';
+  expect(csv(mockData, ';')).toBe(result);
 });
